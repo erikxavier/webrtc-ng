921d4f16f0c289abab66561e1e83bea91f7ed1e7
Erik Xavier <erik.xam@gmail.com>
2016-04-26 07:12:03 -0300

    Front end da tela de solicitação de assistência

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
-var angular = require('angular');
 require('../lib/getScreenId');
+//require('bootstrap');
+var angular = require('angular');
 
-angular.module('WebRTC-NG', [])
-    .controller('CtrlSuporte', require('./controller/suporte.controller'));
+angular.module('WebRTC-NG', [require('angular-ui-router')])
+    .config(function ($stateProvider, $urlRouterProvider) {       
+       //Rota padrão
+       $urlRouterProvider.otherwise("/");
+       
+       $stateProvider
+        .state('main', {
+            url: "/",
+            templateUrl: 'main.html'
+        })
+         .state('solicitar-suporte', {
+             url: '/solicitar-suporte',
+             templateUrl: 'js/suporte/view-suporte-cliente.html',
+             controller: require('./suporte/suporte.controller'),
+             controllerAs: 'vm'
+         }); 
+    });
