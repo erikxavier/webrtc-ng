0321ff04409805a60ed85887cc77c863117f681a
Erik Xavier <erik.xam@gmail.com>
2016-05-17 23:16:40 -0300

    Adicionadas rotas de lista de espera e login, com conexão à base de dados em memória Redis

--- server/api-routes.js
+++ server/api-routes.js
@@ -1,11 +1,14 @@
 var router = require('express').Router();
 var redis = require('redis');
-
+var client = redis.createClient(); 
 
 
 router.post('/solicitar-suporte', function (req, res, next) {
-    var client = redis.createClient();               
-    client.smembers('disponíveis', (err, rply) => {
+    var usuario = req.body;    
+    console.log(usuario);
+    client.sadd('lista-espera', JSON.stringify(usuario));
+                      
+    client.smembers('disponiveis', (err, rply) => {
         if (rply.length > 0) {
             res.send(`Foram encontrados ${rply.length} tecnicos disponíveis`)
         } else {
@@ -14,4 +17,35 @@ router.post('/solicitar-suporte', function (req, res, next) {
     })        
 });
 
+router.get('/lista-espera', function(req, res, next) {
+    client.smembers('lista-espera', (err, reply) => {
+        if (reply.length > 0) {
+            console.log(reply);
+            res.send(reply)
+        } else {
+            res.send('Nenhum chamado pendente');
+        }
+    })     
+})
+
+router.post('/login-tecnico', function (req, res, next) {
+    var usuario = req.body;
+    var response = {};
+    
+    if (validaUsuario(usuario)) {
+        response.autenticado = true;
+        response.token = 'token';
+        client.sadd('disponiveis', usuario.nome);
+        
+    } else {
+        response.autenticado = false;
+    }    
+    res.send(response);
+})
+
 module.exports = router;
+
+
+function validaUsuario(usuario) {
+    return (usuario.nome == 'tecnico' && usuario.senha == '12345');
+}
