937f49f785a3181c6600f1681e740bd3f0d0182d
Erik Xavier <erik.xam@gmail.com>
2016-06-27 23:49:22 -0300

    Removido redis, lista manipulada por array

--- server/api-routes.js
+++ server/api-routes.js
@@ -1,31 +1,45 @@
 var router = require('express').Router();
-var redis = require('redis');
-var client = redis.createClient(); 
+// var redis = require('redis');
+// var client = redis.createClient(); 
+var filaEspera = [];
+var tecnicosDisponiveis = [];
 
 
 router.post('/solicitar-suporte', function (req, res, next) {
     var usuario = req.body;    
-    console.log(usuario);
-    client.sadd('lista-espera', JSON.stringify(usuario));
+    //console.log(usuario);
+    //client.sadd('lista-espera', JSON.stringify(usuario));
+    filaEspera.push(JSON.stringify(usuario));
+
+    if (tecnicosDisponiveis.length) {
+        res.send(`Foram encontrados ${tecnicosDisponiveis.length} técnicos disponíveis.`);
+    } else {
+        res.sendo('Nenhum técnico disponível');
+    }
                       
-    client.smembers('disponiveis', (err, rply) => {
-        if (rply.length > 0) {
-            res.send(`Foram encontrados ${rply.length} tecnicos disponíveis`)
-        } else {
-            res.send('Nenhum técnico disponível');
-        }
-    })        
+    // client.smembers('disponiveis', (err, rply) => {
+    //     if (rply.length > 0) {
+    //         res.send(`Foram encontrados ${rply.length} tecnicos disponíveis`)
+    //     } else {
+    //         res.send('Nenhum técnico disponível');
+    //     }
+    // })        
 });
 
 router.get('/lista-espera', function(req, res, next) {
-    client.smembers('lista-espera', (err, reply) => {
-        if (reply.length > 0) {
-            console.log(reply);
-            res.send(reply)
-        } else {
-            res.send('Nenhum chamado pendente');
-        }
-    })     
+    // client.smembers('lista-espera', (err, reply) => {
+    //     if (reply.length > 0) {
+    //         console.log(reply);
+    //         res.send(reply)
+    //     } else {
+    //         res.send('Nenhum chamado pendente');
+    //     }
+    // })
+    if (filaEspera.length) {
+        res.send(filaEspera);
+    } else {
+        res.send('Nenhum chamado pendente');
+    }    
 })
 
 router.post('/login-tecnico', function (req, res, next) {
@@ -35,7 +49,8 @@ router.post('/login-tecnico', function (req, res, next) {
     if (validaUsuario(usuario)) {
         response.autenticado = true;
         response.token = 'token';
-        client.sadd('disponiveis', usuario.nome);
+        //client.sadd('disponiveis', usuario.nome);
+        tecnicosDisponiveis.push(usuario.nome);
         
     } else {
         response.autenticado = false;
