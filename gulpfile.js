3b1d64a24323c80b0e5bf9b66fe4ac4f491e64a0
Erik Xavier <erik.xam@gmail.com>
2016-07-01 21:56:42 -0300

    Task de distribuição em progresso

--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,58 @@ var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
 var runSequence = require('run-sequence');
 var rename = require('gulp-rename');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var streamify = require('streamify');
 
 gulp.task('clean', function() {
     return gulp.src('dist/')
     .pipe(clean());
 });
 
-gulp.task('jshint', function() {
-    return gulp.src('src/js/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+// gulp.task('jshint', function() {
+//     return gulp.src('src/js/**/*.js')
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'));
+// });
+
+gulp.task('browserify', function() {
+    return browserify('./app/js/main.js')
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .on("error", gutil.log)
+        .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('uglify', function() {
-    return gulp.src(['src/js/**/*.js', 'src/lib/**/*.js'])    
-    .pipe(concat('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
+// gulp.task('uglify', function() {
+//     return gulp.src('./build/temp/app_temp.js')    
+//     .pipe(rename('app.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist/js'));
+// });
 
-gulp.task('htmlmin', function() {
-    return gulp.src('src/view/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist/view'));
-});
+// gulp.task('htmlmin', function() {
+//     return gulp.src('app/js/**/*.html')
+//     .pipe(htmlmin({collapseWhitespace: true}))
+//     .pipe(gulp.dest('dist/view'));
+// });
 
-gulp.task('cssmin', function() {
-    return gulp.src('src/css/**/*.css')
-    .pipe(cleanCSS())
-    .pipe(concat('styles.min.css'))
-    .pipe(gulp.dest('dist/css'));
-});
+// gulp.task('cssmin', function() {
+//     return gulp.src('src/css/**/*.css')
+//     .pipe(cleanCSS())
+//     .pipe(concat('styles.min.css'))
+//     .pipe(gulp.dest('dist/css'));
+// });
 
 gulp.task('copy', function () {
-    return gulp.src('src/index-prod.html')
-    .pipe(rename('index.html'))
+    return gulp.src(['app/lib/**/*.js', 'app/css/**/*.css', 'app/fonts/**/*', 'app/js/**/*.html', 'app/*.html'])
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default', function(cb) {
-    return runSequence('clean', ['jshint', 'uglify', 'htmlmin', 'cssmin', 'copy'], cb)
+    return runSequence('clean', 'browserify', 'copy', cb)
 })
