9f21df7a9b8608c0640c32f9986e2ff6c4bfe7fa
Erik Xavier <erik.xam@gmail.com>
2016-07-01 22:28:32 -0300

    Adicionada compressão dos arquivos no servidor express

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 const certificados = {
     key: fs.readFileSync(__dirname +'/privatekey.key'),
     cert: fs.readFileSync(__dirname +'/certificate.crt')
 };
 
+app.use(compression());
 app.use(express.static('app'));
 
 app.use(bodyParser.json()); // for parsing application/json
